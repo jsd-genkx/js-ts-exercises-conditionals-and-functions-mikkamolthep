8518063afaa9be40de6cfc97223764be5c342e73
mikkamolthep <mik.kamolthep@gmail.com>
2025-11-19 15:17:45 +0700

    task1-2

--- task01.ts
+++ task01.ts
@@ -1,3 +1,12 @@
 // TODO-01: Use if/else to print "Even" or "Odd"
 
-const number = 15;
+// const number = 15;
+// console.log(number)
+let number;
+number = 10;
+
+if (number % 2 === 0) {
+  console.log(number,"is Even")
+} else {
+  console.log(number,"is Odd")
+}
--- task02.ts
+++ task02.ts
@@ -1,5 +1,10 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = NaN;
 // Try changing to "hello", 10, null, NaN
+if (typeof userInput === "number") {
+  console.log('Type is number');
+} else {
+  console.log(typeof userInput);
+}
