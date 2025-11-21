f00bbffb3f1456a71c2701c2ff4d90009e106314
mikkamolthep <mik.kamolthep@gmail.com>
2025-11-21 14:14:08 +0700

    add Fn:fizzBuzz

--- task04.ts
+++ task04.ts
@@ -6,16 +6,16 @@
 
 // TODO BONUS: Rewrite with if
 
-const num = 15;
-if (num % 3 === 0 && num % 5 === 0){  // ใช้ % เพื่อหาเศษจากการหาร 3,5 ได้เศษ 0 คือ ลงตัว
-  console.log("FizzBuzz");
-} else if (num % 3 === 0){    // ถ้าหารแค่ 3 ลงตัวให้ทำงานตามเงื่อนไขนี้
-  console.log("Fizz");
-} else if (num % 5 === 0){    // แต่ถ้าหารแค่ 5 ลงตัวให้ทำงานตามเงื่อนไขนี้
-  console.log("Buzz");
-} else {                      // ถ้าไม่ตรงเงื่อนไขไหนเลยให้ทำงานตามเงื่อนไขนี้
-  console.log("Not divisible by 3 or 5");
-}
+// const num = 15;
+// if (num % 3 === 0 && num % 5 === 0){  // ใช้ % เพื่อหาเศษจากการหาร 3,5 ได้เศษ 0 คือ ลงตัว
+//   console.log("FizzBuzz");
+// } else if (num % 3 === 0){    // ถ้าหารแค่ 3 ลงตัวให้ทำงานตามเงื่อนไขนี้
+//   console.log("Fizz");
+// } else if (num % 5 === 0){    // แต่ถ้าหารแค่ 5 ลงตัวให้ทำงานตามเงื่อนไขนี้
+//   console.log("Buzz");
+// } else {                      // ถ้าไม่ตรงเงื่อนไขไหนเลยให้ทำงานตามเงื่อนไขนี้
+//   console.log("Not divisible by 3 or 5");
+// }
 
 // TODO BONUS: Rewrite with if 
 // ใช้แค่ if คล้ายๆข้างบน แค่ตั้งเงื่อนไขใหม่ในทุกๆ if
@@ -46,3 +46,26 @@ if (num % 3 === 0 && num % 5 === 0){  // ใช้ % เพื่อหาเศ
 // } else {
 //   console.log("Not divisible by 3 or 5");
 // }
+
+const fizzBuzz = (number:number) => {
+  if (number % 3 === 0 && number % 5 === 0){
+    console.log("FizzBuzz");
+  } else if (number % 3 === 0){
+    console.log("Fizz");
+  } else if (number % 5 === 0){
+    console.log("Buzz");
+  } else {
+    console.log("Not divisible by 3 or 5");
+  }
+};
+
+fizzBuzz(15);
+
+const fizzBuzz2 = fizzBuzz;
+fizzBuzz2(20);
+
+// Example Test Cases
+fizzBuzz(15); // "FizzBuzz"
+fizzBuzz(9); // "Fizz"
+fizzBuzz(10); // "Buzz"
+fizzBuzz(7); // "Not divisible by 3 or 5"
